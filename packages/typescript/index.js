cf807a50eed9ea2e0b347eb77452b7b2748797b9
yangss3 <yss_2016@outlook.com>
2021-05-15 17:54:53 +0800

    feat: add lint configuration

---
+++ packages/typescript/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    '@yangss/eslint-config-basic',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
---
+++ packages/vue/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@yangss/eslint-config-ts'
+  ],
+  rules: {
+  }
+}
