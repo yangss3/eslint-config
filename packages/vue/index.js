67f8025bed38dd4221b12efbb2103e77e9e9db39
yangss3 <yss_2016@outlook.com>
2021-05-16 14:53:05 +0800

    feat: update eslint-config-vue

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,6 +3,17 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     '@yangss/eslint-config-ts'
   ],
+  overrides: {
+    files: ['*.vue'],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    }
+  },
   rules: {
   }
 }
