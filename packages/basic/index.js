f2c03c8bfc48b04020442353ecb819897392ae4c
yangss3 <yss_2016@outlook.com>
2022-01-26 00:16:43 +0800

    update config

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -5,15 +5,17 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
+    indent: ['error', 'tab'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
     'no-var': 'error',
+    'linebreak-style': ['error', 'unix'],
     'no-multiple-empty-lines': ['error', { max: 2 }]
   },
   ignorePatterns: ['dist/*']
